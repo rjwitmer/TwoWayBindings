b95eafe17fee9d1262233b63575f3c59adefde7d
Bob Witmer <bwitmer@golden.net>
2023-06-30 14:48:22 -0400

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Bob Witmer on 2023-06-30.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
